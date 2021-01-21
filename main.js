84f1b76931094b89f4421aeaf7db97b66bdd2e7d
diego manuel guerrero torres <diegomgt11@gmail.com>
2021-01-20 20:10:39 -0500

    primeros_Pasos_JS_20_01_2021

---
+++ main.js
@@ -0,0 +1,32 @@
+
+console.log("hola mundo");
+
+// tipos de datos
+
+//  string cadena 
+"diego guerrero"
+
+// numericos
+
+2.7 -25
+ 
+
+// bolean afirmaciones
+
+true
+false
+
+// lista o arrays
+
+["diego","felipe"]
+
+
+// objetos
+
+{
+nombre:"diego",
+apeliido:"guerrero",
+edad:14,
+profesor:null
+
+}
